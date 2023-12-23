b08bea20a0e444f335669ac3c3e5e2a2a9c22205
김현우 <56820507+starry-asterisk@users.noreply.github.com>
2023-12-24 02:32:39 +0900

    mask-image

--- exam/index.js
+++ exam/index.js
@@ -226,10 +226,15 @@ function frameHandler(frame){
                         object._img.src = `./resource/imageSet/${option.name}/${option.type}.png`;
                         object._instance.appendChild(object._img);
                         Player.object_container.appendChild(object._instance);
-                        Player.object_container.style.maskImage = `url(./resource/imageSet/${option.name}/${option.type}.png)`;
+                        Player._img.style.maskImage = `url(./resource/imageSet/${option.name}/${option.type}.png)`;
+                        Player._img.style.maskSize = `contain`;
+                        Player._img.style.maskOrigin = `center`;
                     }else{
                         option = {...object, ...option};
-                        object._img.src = `./resource/imageSet/${option.name}/${option.type}.png`;                    
+                        object._img.src = `./resource/imageSet/${option.name}/${option.type}.png`;     
+                        Player._img.style.maskImage = `url(./resource/imageSet/${option.name}/${option.type}.png)`;
+                        Player._img.style.maskSize = `contain`;
+                        Player._img.style.maskOrigin = `center`;               
                     }
 
                     if(option.horizontal_invert===true)object._instance.classList.add('horizontal_invert');
